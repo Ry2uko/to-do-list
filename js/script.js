38f6d64f85942e7a1ed106eb6b6aea65d7ecf1ed
Ry2uko <83095832+Ry2uko@users.noreply.github.com>
2022-02-10 09:18:17 +0800

    New Folder Structure + Bootstrap and jQuery
    New folder structure and production Bootstrap and jQuery

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// Hacked by @Ryuko :D
+$(function(){
+  
+  $('#addToDo').click(function(){
+    const inputVal = $('#toDoInput').val();
+    
+    if (inputVal === '') return;
+    
+    $('#listBody ul').append(`
+    <label class="list-group-item py-2 ps-3 d-flex">
+      <span class="user-select-none">${inputVal}</span>
+      <input type="checkbox" class="form-check-input ms-auto me-1 mt-1 shadow-none" />
+    </label>
+    `);
+    
+    $('input[type="checkbox"]').change(function(){
+      const listText = $(this).prev()[0];
+      const textVal = $(listText).text();
+      if (this.checked) {  
+        $(listText).html(`<del class="text-muted">${textVal}</del>`)
+      } else {
+        $(listText).html(`${textVal}`)
+      }
+    });
+    
+    $('#toDoInput').val('');
+  });
+  
+  // saved list
+  $('input[type="checkbox"]').change(function(){
+      const listText = $(this).prev()[0];
+      const textVal = $(listText).text();
+      if (this.checked) {  
+        $(listText).html(`<del class="text-muted">${textVal}</del>`)
+      } else {
+        $(listText).html(`${textVal}`)
+      }
+    });
+  
+  $('#toDoInput').keypress(function(e){
+    if (e.key === 'Enter') $('#addToDo').click();
+  });
+});
