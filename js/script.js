378e6f75c302212344960ffcb5a1ba3ebb2b7ef8
Ry2uko <83095832+Ry2uko@users.noreply.github.com>
2022-02-11 10:36:04 +0800

    Task saves (localStorage)
    1. Tasks can now be saved using localStorage
    2. Each tasks have their own uniquely generated id

--- js/script.js
+++ js/script.js
@@ -16,6 +16,18 @@ $(function(){
     }
   }
 
+  // Generate unique id for each task even if they have the same name
+  function generateId() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '', length = 7;
+
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * length));
+    }
+
+    return result;
+  }
+
   // Add Event listeners; since we'll be creating new elements
   function addListeners() {
     
@@ -26,25 +38,79 @@ $(function(){
     $('input[type="checkbox"]').change(function(){
       if (editMode) return;
 
+      const taskId = $(this).parent().attr('id');
       const listText = $(this).prev()[0];
       const inputVal = $(listText).text();
+
+      const savedList = localStorage.getItem('savedList');
+      const parsedList = JSON.parse(savedList);
+      
       if (this.checked) {  
-        $(listText).html(`<span class="text-muted done task-name"<del>${inputVal}</del></span>`)
+        $(listText).addClass('text-muted done');
+        $(listText).html(`<del>${inputVal}</del>`);
+        for (let i = 0; i < parsedList.length; i++) {
+          if (parsedList[i].id === taskId) {
+            parsedList[i].done = true;
+            break;
+          }
+        }
       } else {
-        $(listText).html(`<span class="task-name">${inputVal}</span>`)
+        $(listText).removeClass('text-muted done');
+        $(listText).html(inputVal);
+        for (let i = 0; i < parsedList.length; i++) {
+          if (parsedList[i].id === taskId) {
+            parsedList[i].done = false;
+            break;
+          }
+        }
       }
+
+      localStorage.setItem('savedList', JSON.stringify(parsedList));
       changeCount();
     });
 
     $('.delete-task').click(function(){
+      const savedList = localStorage.getItem('savedList');
+      const parsedList = JSON.parse(savedList);
+      const newList = [...parsedList];
+      const taskId = $(this).parent().attr('id');
+
+      for (let i = 0; i < newList.length; i++) {
+        if (newList[i].id === taskId) {
+          parsedList.splice(i, 1);
+          break;
+        }
+      }
+
       $(this).parent().remove();
+      localStorage.setItem('savedList', JSON.stringify(parsedList));
       changeCount();
+    });
+  }
+
+  // Saved List (localStorage)
+  const savedList = localStorage.getItem('savedList');
+  if (savedList) {
+    const parsedList = JSON.parse(savedList);
+    parsedList.map(task => {
+      $('#listBody div#tasksContainer').append(`
+        <label class="list-group-item py-2 ps-3 d-flex" id="${task.id}">
+          ${ 
+            task.done 
+              ? `<span class="text-muted done task-name"><del>${task.name}</del></span>` 
+              : `<span class="task-name">${task.name}</span>`
+          }
+          <input type="checkbox" class="form-check-input ms-auto me-1 shadow-none task-done d-block" ${task.done ? "checked" : ""}/> 
+          <input type="image" src="img/icon/xmark-solid.svg" class="delete-task d-none" alt="delete task" />
+        </label>
+      `);
     });
   }
 
   // Edit and Delete
   $('#deleteAll').click(function(){
     $('#listBody div#tasksContainer').empty();
+    localStorage.removeItem('savedList');
     changeCount();
   })
   $('#editTasks').click(function(){
@@ -76,18 +142,30 @@ $(function(){
   // Adding Tasks
   $('#addToDo').click(function(){
     const inputVal = $('#toDoInput').val();
+    const eleId = generateId();
     if (inputVal === '') return;
+
     $('#listBody div#tasksContainer').append(`
-    <label class="list-group-item py-2 ps-3 d-flex">
-      <span class="task-name">${inputVal}</span>
-      ${editMode 
-        ? '<input type="checkbox" class="form-check-input ms-auto me-1 shadow-none task-done d-none" disabled/> \
-        <input type="image" src="img/icon/xmark-solid.svg" class="delete-task d-block" alt="delete task" />' 
-        : '<input type="checkbox" class="form-check-input ms-auto me-1 shadow-none task-done d-block" /> \
-        <input type="image" src="img/icon/xmark-solid.svg" class="delete-task d-none" alt="delete task" />'}
-    </label>
+      <label class="list-group-item py-2 ps-3 d-flex" id="${eleId}">
+        <span class="task-name">${inputVal}</span>
+        ${editMode 
+          ? '<input type="checkbox" class="form-check-input ms-auto me-1 shadow-none task-done d-none" disabled/> \
+          <input type="image" src="img/icon/xmark-solid.svg" class="delete-task d-block" alt="delete task" />' 
+          : '<input type="checkbox" class="form-check-input ms-auto me-1 shadow-none task-done d-block" /> \
+          <input type="image" src="img/icon/xmark-solid.svg" class="delete-task d-none" alt="delete task" />'}
+      </label>
     `);
-    
+
+    // Saving to Local Storage
+    const savedList = localStorage.getItem('savedList') || JSON.stringify([]);
+    const parsedList = JSON.parse(savedList);
+    parsedList.push({
+      id: eleId,
+      name: inputVal,
+      done: false
+    });
+    localStorage.setItem('savedList', JSON.stringify(parsedList));
+
     addListeners();
     changeCount();
 
